a38dbe55e892203acd8001df974939e74d77f225
Tim Garrett <tgarrett@Tims-MacBook-Pro.local>
2022-11-09 20:24:48 -0600

    Integrate CHIP-0008 puzzle + 1.6.1 Chia fixes + add memo support

--- royalty_share/tests/test_p2_royalty_share_arbitrary_shares.py
+++ royalty_share/tests/test_p2_royalty_share_arbitrary_shares.py
@@ -13,6 +13,7 @@ from chia.types.condition_opcodes import ConditionOpcode
 from chia.types.coin_spend import CoinSpend
 from chia.types.spend_bundle import SpendBundle
 from chia.util.ints import uint64
+from chia.wallet.cat_wallet.cat_utils import CAT_MOD
 
 import cdv.clibs as std_lib
 from cdv.test import CoinWrapper, SpendResult
@@ -20,7 +21,7 @@ from cdv.test import setup as setup_test
 from cdv.util.load_clvm import load_clvm
 
 clibs_path: Path = Path(std_lib.__file__).parent
-ROYALTY_MOD: Program = load_clvm("p2_royalty_share_allocated_percentages_feeless.clsp", "clsp.archives.p2_royalty_share_allocated_percentages_feeless", search_paths=[clibs_path])
+ROYALTY_MOD_REST_TO_LAST: Program = load_clvm("p2_royalty_share_arbitrary_shares_rest_to_last.clsp", "clsp.p2_royalty_share_arbitrary_shares", search_paths=[clibs_path])
 
 class TestP2RoyaltyShareAllocatedPercentagesFeeless:
 
@@ -31,16 +32,16 @@ class TestP2RoyaltyShareAllocatedPercentagesFeeless:
         yield network, alice, bob
 
     @pytest.mark.asyncio
-    async def test_puzzle_50_50_no_tip(self, setup):
+    async def test_puzzle_50_50_no_remainder(self, setup):
         network, alice, bob = setup
         await(network.farm_block(farmer=alice))
         
         f00d = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafef00d')
         beef = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebeef')
-        babe = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebabe')
+        burn = bytes32.fromhex('000000000000000000000000000000000000000000000000000000000000dead')
 
         try:
-            royalty_puzzle: Program = ROYALTY_MOD.curry([[f00d, 5000], [beef, 5000]], babe)
+            royalty_puzzle: Program = ROYALTY_MOD_REST_TO_LAST.curry([[f00d, 5000], [beef, 5000]])
             royalty_amount = uint64(10000)
 
             allocation_spend_result = await self.do_royalty_spends(network, alice, royalty_puzzle, royalty_amount)
@@ -66,66 +67,71 @@ class TestP2RoyaltyShareAllocatedPercentagesFeeless:
             await network.close()    
 
     @pytest.mark.asyncio
-    async def test_puzzle_50_50_remainder_to_tip_jar(self, setup):
+    async def test_puzzle_50_50_remainder_to_last(self, setup):
         network, alice, bob = setup
         await(network.farm_block(farmer=alice))
         
         f00d = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafef00d')
         beef = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebeef')
-        babe = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebabe')
 
         try:
-            royalty_puzzle: Program = ROYALTY_MOD.curry([[f00d, 5000], [beef, 5000]], babe)
+            royalty_puzzle: Program = ROYALTY_MOD_REST_TO_LAST.curry([[f00d, 5000], [beef, 5000]])
             royalty_amount = uint64(10001)
 
             allocation_spend_result = await self.do_royalty_spends(network, alice, royalty_puzzle, royalty_amount)
 
             allocation_spend_coins = allocation_spend_result["additions"]
-            assert 3 == len(allocation_spend_coins)
+            assert 2 == len(allocation_spend_coins)
             allocation_f00d: Coin = None
             allocation_beef: Coin = None
-            allocation_babe: Coin = None
 
             for coin in allocation_spend_coins:
                 if coin.puzzle_hash == f00d:
                     allocation_f00d = coin
                 elif coin.puzzle_hash == beef:
                     allocation_beef = coin
-                elif coin.puzzle_hash == babe:
-                    allocation_babe = coin
 
             assert allocation_f00d is not None
             assert allocation_f00d.amount == uint64(5000)
 
             assert allocation_beef is not None
-            assert allocation_beef.amount == uint64(5000)
-
-            assert allocation_babe is not None
-            assert allocation_babe.amount == uint64(1)
+            assert allocation_beef.amount == uint64(5000 + 1)
 
         finally:
             await network.close()    
 
     @pytest.mark.asyncio
-    async def test_puzzle_dusting_to_tip_jar(self, setup):
+    async def test_puzzle_dusting_to_last(self, setup):
         network, alice, bob = setup
         await(network.farm_block(farmer=alice))
         
         f00d = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafef00d')
         beef = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebeef')
-        babe = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebabe')
 
         try:
-            royalty_puzzle: Program = ROYALTY_MOD.curry([[f00d, 5000], [beef, 5000]], babe)
+            royalty_puzzle: Program = ROYALTY_MOD_REST_TO_LAST.curry([[f00d, 5000], [beef, 5000]])
             royalty_amount = uint64(1)
 
             allocation_spend_result = await self.do_royalty_spends(network, alice, royalty_puzzle, royalty_amount)
 
             allocation_spend_coins = allocation_spend_result["additions"]
-            assert 1 == len(allocation_spend_coins)
-            allocation_babe: Coin = allocation_spend_coins[0]
-            assert allocation_babe.puzzle_hash == babe
-            assert allocation_babe.amount == uint64(1)
+            assert 2 == len(allocation_spend_coins)
+            
+            allocation_f00d: Coin = None
+            allocation_beef: Coin = None
+
+            for coin in allocation_spend_coins:
+                if coin.puzzle_hash == f00d:
+                    allocation_f00d = coin
+                elif coin.puzzle_hash == beef:
+                    allocation_beef = coin
+
+            assert allocation_f00d is not None
+            assert allocation_f00d.amount == uint64(0)
+
+            assert allocation_beef is not None
+            assert allocation_beef.amount == uint64(1)
+
 
         finally:
             await network.close()    
@@ -159,10 +165,19 @@ class TestP2RoyaltyShareAllocatedPercentagesFeeless:
                 royalty_coin_to_spend = coin
         assert royalty_coin_to_spend.puzzle_hash == royalty_puzzle_hash
  
+        mod, curried_args = royalty_puzzle.uncurry()
+        if mod == CAT_MOD:
+            mod, curried_args = curried_args.at("rrf").uncurry()
+        payout_scheme = curried_args.first()
+
+        total_shares = 0
+        for entry in payout_scheme.as_iter():
+            total_shares += entry.rest().first().as_int()
+        
         allocation_spend = CoinSpend(
             royalty_coin_to_spend,
             royalty_puzzle,
-            Program.to([royalty_amount])
+            Program.to([royalty_amount, total_shares])
         )
         allocation_spend_bundle = SpendBundle(
             [allocation_spend],
@@ -182,10 +197,9 @@ class TestP2RoyaltyShareAllocatedPercentagesFeeless:
         f001 = bytes32.fromhex('000000000000000000000000000000000000000000000000000000000000f001')
         f002 = bytes32.fromhex('000000000000000000000000000000000000000000000000000000000000f002')
         f003 = bytes32.fromhex('000000000000000000000000000000000000000000000000000000000000f003')
-        babe = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebabe')
 
         try:
-            royalty_puzzle: Program = ROYALTY_MOD.curry([[f001, 8000], [f002, 1500], [f003, 500]], babe)
+            royalty_puzzle: Program = ROYALTY_MOD_REST_TO_LAST.curry([[f001, 8000], [f002, 1500], [f003, 500]])
             royalty_amount = uint64(1000000)
 
             allocation_spend_result = await self.do_royalty_spends(network, alice, royalty_puzzle, royalty_amount)
@@ -216,5 +230,116 @@ class TestP2RoyaltyShareAllocatedPercentagesFeeless:
         finally:
             await network.close()    
 
+    @pytest.mark.asyncio
+    async def test_invalid_amount(self, setup):
+        network, alice, bob = setup
+        await(network.farm_block(farmer=alice))
+        
+        f00d = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafef00d')
+        beef = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebeef')
+
+        try:
+            royalty_puzzle: Program = ROYALTY_MOD_REST_TO_LAST.curry([[f00d, 5000], [beef, 5000]])
+            royalty_amount = uint64(10000)
+            invalid_amount = royalty_amount - uint64(5000)
+
+            royalty_puzzle_hash = royalty_puzzle.get_tree_hash()
+
+            #send funds to royalty puzzle, emulating the sale of an NFT
+            royalty_coin: Optional[CoinWrapper] = await alice.choose_coin(royalty_amount)
+            royalty_spend_result: SpendResult = await alice.spend_coin(royalty_coin, 
+                pushTx=True,
+                amt=royalty_amount,
+                custom_conditions=[
+                    [
+                        ConditionOpcode.CREATE_COIN,
+                        royalty_puzzle_hash,
+                        royalty_amount
+                    ],
+                    [
+                        ConditionOpcode.CREATE_COIN,
+                        royalty_coin.puzzle_hash,
+                        (royalty_coin.amount - royalty_amount)
+                    ]
+                ])
+
+            assert royalty_spend_result.error is None
+            assert len(royalty_spend_result.outputs) == 2
+            royalty_coin_to_spend = None
+            for coin in royalty_spend_result.outputs:
+                if coin.puzzle_hash == royalty_puzzle_hash:
+                    royalty_coin_to_spend = coin
+            assert royalty_coin_to_spend.puzzle_hash == royalty_puzzle_hash
+    
+            allocation_spend = CoinSpend(
+                royalty_coin_to_spend,
+                royalty_puzzle,
+                Program.to([invalid_amount, 10000])
+            )
+            allocation_spend_bundle = SpendBundle(
+                [allocation_spend],
+                G2Element(),
+            )
+
+            allocation_spend_result = await network.push_tx(allocation_spend_bundle)
+            assert "error" in allocation_spend_result
+
+        finally:
+            await network.close()   
 
 
+    @pytest.mark.asyncio
+    async def test_invalid_total_shares(self, setup):
+        network, alice, bob = setup
+        await(network.farm_block(farmer=alice))
+        
+        f00d = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafef00d')
+        beef = bytes32.fromhex('00000000000000000000000000000000000000000000000000000000cafebeef')
+
+        try:
+            royalty_puzzle: Program = ROYALTY_MOD_REST_TO_LAST.curry([[f00d, 5000], [beef, 5000]])
+            royalty_amount = uint64(10000)
+
+            royalty_puzzle_hash = royalty_puzzle.get_tree_hash()
+
+            #send funds to royalty puzzle, emulating the sale of an NFT
+            royalty_coin: Optional[CoinWrapper] = await alice.choose_coin(royalty_amount)
+            royalty_spend_result: SpendResult = await alice.spend_coin(royalty_coin, 
+                pushTx=True,
+                amt=royalty_amount,
+                custom_conditions=[
+                    [
+                        ConditionOpcode.CREATE_COIN,
+                        royalty_puzzle_hash,
+                        royalty_amount
+                    ],
+                    [
+                        ConditionOpcode.CREATE_COIN,
+                        royalty_coin.puzzle_hash,
+                        (royalty_coin.amount - royalty_amount)
+                    ]
+                ])
+
+            assert royalty_spend_result.error is None
+            assert len(royalty_spend_result.outputs) == 2
+            royalty_coin_to_spend = None
+            for coin in royalty_spend_result.outputs:
+                if coin.puzzle_hash == royalty_puzzle_hash:
+                    royalty_coin_to_spend = coin
+            assert royalty_coin_to_spend.puzzle_hash == royalty_puzzle_hash
+    
+            allocation_spend = CoinSpend(
+                royalty_coin_to_spend,
+                royalty_puzzle,
+                Program.to([royalty_amount, 5000])  # correct would be 10000
+            )
+            allocation_spend_bundle = SpendBundle(
+                [allocation_spend],
+                G2Element(),
+            )
+
+            allocation_spend_result = await network.push_tx(allocation_spend_bundle)
+            assert "error" in allocation_spend_result
+
+        finally:
+            await network.close()  
